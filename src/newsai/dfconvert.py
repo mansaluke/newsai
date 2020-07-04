c179131c21f5516493857da7879852ee610472e1
mansaluke <lukemcleary95@gmail.com>
2020-07-04 19:13:53 +0200

    add mkdir function

--- src/newsai/dfconvert.py
+++ src/newsai/dfconvert.py
@@ -186,3 +186,34 @@ class Dstore(object):
         # TODO update key
         dataframe.to_hdf(filename, key='df1', append=True,
                          mode='w', format='t',  data_columns=True)
+
+
+def mkdir_p(path):
+    import errno
+    try:
+        path = path
+        makedirs(path)
+        return path
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and isdir(path):
+            return path
+        else:
+            raise
+
+
+def create_path(path):
+    if not exists(path):
+        a = True
+        while a is True:
+            user_input = input(
+                path +
+                ' does not exist. Create folder? (y/n): '
+            ).casefold()
+            if user_input == 'y':
+                path = mkdir_p(path)
+            elif user_input == 'n':
+                pass
+            a = False
+    else:
+        pass
+    return path
